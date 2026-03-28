c60b1d2714c1912e27c5728defa8bf5d4b953268
ABDELRAHMAN ELSAYED <abdoelhosin250@gmail.com>
2026-03-28 19:02:27 +0300

    ADD

--- debug-prisma.ts
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const client = new PrismaClient();
-console.log('Client keys:', Object.keys(client));
-console.log('Client options keys:', Object.keys((client as any)._options || {}));
